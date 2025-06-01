bbd2f6b45d004759eb80a616b8af19381030b459
hayshin <hayshinbj@gmail.com>
2025-06-01 10:52:19 +0500

    changed some colors

--- home/hayshin/optional/desktops/window_managers/hypr/settings/general.nix
+++ home/hayshin/optional/desktops/window_managers/hypr/settings/general.nix
@@ -3,8 +3,8 @@
   gaps_in = 0;
   gaps_out = 0;
   border_size = 2;
-  "col.active_border" = "rgb(${config.lib.stylix.colors.base02})";
-  "col.inactive_border" = "rgb(${config.lib.stylix.colors.base00})";
+  "col.active_border" = "rgb(${config.lib.stylix.colors.base03})";
+  "col.inactive_border" = "rgb(${config.lib.stylix.colors.base01})";
   layout = "dwindle";
   # cursor_inactive_timeout = 5;
   resize_on_border = true;
