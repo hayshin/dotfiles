15f899b7746dbb77a17780c2186133ff47ea7ea8
hayshin <hayshinbj@gmail.com>
2025-08-10 12:39:11 +0500

    fix: remove biome lsp

--- home/hayshin/common/helix/languages.nix
+++ home/hayshin/common/helix/languages.nix
@@ -15,62 +15,6 @@
       };
       language-servers = [
         "pyright"
-        "basedpyright"
-      ];
-    }
-    {
-      name = "javascript";
-      auto-format = true;
-      language-servers = [
-        "biome"
-        "typescript-language-servers"
-      ];
-    }
-    {
-      name = "typescript";
-      auto-format = true;
-      language-servers = [
-        "biome"
-        "typescript-language-servers"
-      ];
-    }
-    {
-      name = "tsx";
-      auto-format = true;
-      language-servers = [
-        "biome"
-        "typescript-language-servers"
-      ];
-    }
-    {
-      name = "json";
-      auto-format = true;
-      language-servers = [
-        "biome"
-        "typescript-language-servers"
-      ];
-    }
-    {
-      name = "jsonc";
-      auto-format = true;
-      language-servers = [
-        "biome"
-        "typescript-language-servers"
-      ];
-    }
-    {
-      name = "css";
-      auto-format = true;
-      language-servers = [
-        "biome"
-        "typescript-language-servers"
-      ];
-    }
-    {
-      name = "svelte";
-      auto-format = true;
-      language-servers = [
-        "typescript-language-servers"
       ];
     }
   ];
