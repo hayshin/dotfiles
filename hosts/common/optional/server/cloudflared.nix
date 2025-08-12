1037afc2c2809824f226a9039f263485c8089c28
hayshin <hayshinbj@gmail.com>
2025-08-12 18:59:08 +0500

    checkpoint

--- hosts/common/optional/server/cloudflared.nix
+++ hosts/common/optional/server/cloudflared.nix
@@ -3,9 +3,12 @@
   services.cloudflared = {
     enable = true;
     tunnels = {
-      "00000000-0000-0000-0000-000000000000" = {
-        credentialsFile = "/home/hayshin/.cloudflared/e27bd15c-a906-407f-a2d2-6aa5dd5b8079.json";
+      "a57101b4-90c6-4ec8-a065-967552c12949" = {
+        credentialsFile = "/home/hayshin/.cloudflared/a57101b4-90c6-4ec8-a065-967552c12949.json";
         default = "http_status:404";
+        ingress = {
+          "meeplix.hayshin.dev" = "http://127.0.0.1:3000";
+        };
       };
     };
   };
