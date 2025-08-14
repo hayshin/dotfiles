7c722cfd9b341f336f7e658334473872ed506c04
hayshin <hayshinbj@gmail.com>
2025-08-14 11:38:01 +0500

    feat: xdg mime types

--- home/hayshin/optional/desktops/window_managers/common/xdg.nix
+++ home/hayshin/optional/desktops/window_managers/common/xdg.nix
@@ -5,18 +5,61 @@ in
 {
   xdg = {
     enable = true;
+    desktopEntries = {
+      browser = {
+        name = "browser";
+        genericName = "Browser selector";
+        exec = "vivaldi";
+        type = "Application";
+        terminal = false;
+        categories = [
+          "Application"
+          "Network"
+          "WebBrowser"
+        ];
+        mimeType = [
+          "text/html"
+          "text/xml"
+          "application/pdf"
+        ];
+      };
+      # Telegram Desktop not opens helix editor because it terminal
+      editor = {
+        name = "editor";
+        genericName = "Editor selector";
+        exec = "zeditor";
+        type = "Application";
+        terminal = false;
+      };
+      image = {
+        name = "image";
+        genericName = "Image selector";
+        exec = "imv";
+        type = "Application";
+        terminal = false;
+      };
+    };
     mimeApps = {
       enable = true;
-      # associations.added = {
-      #   "application/pdf" = "zen-browser.desktop";
-      # };
       defaultApplications = {
-        "application/pdf" = "zen.desktop";
-        "x-scheme-handler/http" = "zen.desktop";
-        "x-scheme-handler/https" = "zen.desktop";
-        "image/jpeg" = "imv.desktop";
-        # "image/jpeg" = "imv.desktop";
-        "image/png" = "imv.desktop";
+        "text/plain" = "editor.desktop";
+        "text/markdown" = "editor.desktop";
+
+        "text/html" = "browser.desktop";
+        "x-scheme-handler/http" = "browser.desktop";
+        "x-scheme-handler/https" = "browser.desktop";
+        "x-scheme-handler/about" = "browser.desktop";
+        "x-scheme-handler/unknown" = "browser.desktop";
+
+        "x-scheme-handler/tg" = [ "org.telegram.desktop.desktop " ];
+
+        "application/pdf" = "browser.desktop";
+        "application/octet-stream" = "browser.desktop";
+
+        "image/jpeg" = "image.desktop";
+        "image/jpg" = "image.desktop";
+        "image/svg" = "image.desktop";
+        "image/png" = "image.desktop";
       };
     };
     userDirs = {
@@ -30,27 +73,12 @@ in
       templates = "${home}/templates";
       publicShare = "${home}/public";
     };
-    desktopEntries = {
-      zen = {
-        name = "Zen Browser";
-        genericName = "Web Browser";
-        exec = "zen";
-        terminal = false;
-        categories = [
-          "Application"
-          "Network"
-          "WebBrowser"
-        ];
-        mimeType = [
-          "text/html"
-          "text/xml"
-          "application/pdf"
-        ];
-      };
-    };
     portal = {
       extraPortals = with pkgs; [
         xdg-desktop-portal-termfilechooser
+        xdg-desktop-portal-hyprland
+        xdg-desktop-portal
+        xdg-desktop-portal-wlr
       ];
       config = {
         common."org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
--- hosts/common/optional/server/default.nix
+++ hosts/common/optional/server/default.nix
@@ -1,6 +1,5 @@
 {
   imports = [
-    ./cloudflared.nix
     ./minecraft.nix
     ./ssh.nix
   ];
